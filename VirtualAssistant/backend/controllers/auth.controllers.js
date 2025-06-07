bcd4127b822ae8c5744b0a2f547ef3aa9fb9c2dd
simran <simransingh956429@gmail.com>
2025-06-07 22:46:30 +0530

    two

--- VirtualAssistant/backend/controllers/auth.controllers.js
+++ VirtualAssistant/backend/controllers/auth.controllers.js
@@ -1,6 +1,20 @@
+import User from "../models/user.model.js"
+
 export const signUp=async (req,res)=>{
     try {
         const {name,email,password}=req.body
+
+        const existEmail = await User.findOne({ email });
+        if(existEmail){
+            return res.status(400).json({message:"Email already exists"})
+        }
+
+        if(password.length<6){
+            return res.status(400).json({message:"Password must be atleast 6 characters long "})
+        }
+
+
+
         
     } catch (error) {
         
